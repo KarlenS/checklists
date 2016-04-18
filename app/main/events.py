2501467031d8c0c3a3b5f63edbbbec3bfe4dfb6c
karlenS <shkarlen@gmail.com>
2016-04-17 23:04:12 -0500

    Initial commit; site has functional sockets for checkboxes and progress bar TODO: storing info, including completed form (potentially in mysql database)

---
+++ app/main/events.py
@@ -0,0 +1,50 @@
+from flask import session
+from flask.ext.socketio import emit, join_room, leave_room
+from .. import socketio
+
+
+@socketio.on('joined', namespace='/startnightform')
+def joined(message):
+    """Sent by clients when they enter a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('check', namespace='/startnightform')
+def uncheck(message):
+    """Sent by a client when the user checks a box.
+    The box is checked for all people in the room."""
+    room = session.get('room')
+    sendID = '#'+message['msg'] 
+    emit('unmessage', {'msg': sendID}, room=room)
+    print "EMITTING uncheck for",sendID
+
+
+@socketio.on('uncheck', namespace='/startnightform')
+def check(message):
+    """Sent by a client when the user checks a box.
+    The box is checked for all people in the room."""
+    room = session.get('room')
+    sendID = '#' + message['msg']
+    emit('message', {'msg': sendID}, room=room)
+    print "EMITTING check for", sendID
+
+@socketio.on('updatebar', namespace='/startnightform')
+def updatebar(message):
+    """Sent by a client when the user checks a box.
+    The progress bar is updated for everyone."""
+    room = session.get('room')
+    sendBar = message['bar']
+    emit('pbar', {'bar': sendBar}, room=room)
+    print "EMITTING new bar value for", sendBar
+
+@socketio.on('left', namespace='/startnightform')
+def left(message):
+    """Sent by clients when they leave a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    leave_room(room)
+    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
+
---
+++ app/templates/updateBar.js
@@ -0,0 +1,69 @@
+var main = function() {
+    var getMessage = function (i) {
+    var msgs = ['Another one!',
+                'You smart.',
+                'You very smart!',
+                'We da best!',
+                'You a genius!',
+                'I appreciate you.',
+                'You loyal.',
+                'I changed ALOT. You can too :)',
+                'WIN WIN WIN no matter what!',
+                'Never give up, never surrender.',
+                'Put this money in your savings account.',
+                'Buy yo mama a house.',
+                'Buy your whole family houses.',
+                'Another one!',
+                'Another one!',
+                'Another one!',
+            ];
+
+            return msgs[i];
+        };
+
+    $('input').on('click', function(){
+
+        toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-bottom-full-width",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "2000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+        }   
+
+
+    if(this.checked){
+      var i = Math.floor((Math.random() * 15));
+      var msg = getMessage(i);
+      toastr.success(msg);
+       
+    }
+    else {
+      toastr.error('NOOOOOOOOOO!');
+    }
+    //if(value == 126) 
+
+
+    //toastr.error('FUCK?!');
+  });
+    $('#reset').click(function (){
+      $("#progress-bar")
+      .css("width", "0%")
+      .attr("aria-valuenow", '0')
+      .text("0/126");
+    })
+    
+  $(':checkbox:checked').prop('checked',false);//this clears checkboxes on refresh
+}
+
+$(document).ready(main);
