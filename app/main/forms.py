82fa101cdca15cf2b981d5711a23f9aa38f7a97c
KarlenS <shkarlen@gmail.com>
2016-05-01 19:06:51 -0500

    Fixed #14 (untested on mobile).

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,12 +1,13 @@
 from flask.ext.wtf import Form
+from wtforms.ext.dateutil.fields import DateField
 from wtforms.fields import StringField, SubmitField
-from wtforms.fields.html5 import DateField
+#from wtforms.fields.html5 import DateField
 from wtforms.validators import Required,Optional
 
 class LoginForm(Form):
     """Accepts a nickname and a room."""
     name = StringField('Observer', validators=[Required()])
-    date = DateField('Date (UTC)', validators=[Required()])
+    date = DateField('Date (UTC)', validators=[Required()], display_format='%Y-%m-%d')
     room = StringField('Room', validators=[Optional()])
     submit_start = SubmitField('Start of Night Form')
     submit_end = SubmitField('End of Night Form')
